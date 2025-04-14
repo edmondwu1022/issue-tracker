7ab5306b30ceb42522e3461db2140e37a3ce4426
Edmond <edmondwu1022@gmail.com>
2025-04-14 17:38:36 +0800

    Handle form submission

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,16 +1,47 @@
 "use client"
 import styles from "./newIssue.module.css"
-import { Button, TextArea, TextField } from "@radix-ui/themes"
+import { Button, TextArea, TextField, } from "@radix-ui/themes"
 import SimpleMDE from "react-simplemde-editor"
 import "easymde/dist/easymde.min.css"
+import { Controller, useForm } from "react-hook-form"
+import { useRouter } from "next/navigation"
+
+interface IssueForm {
+    title: string,
+    description: string
+}
 
 const NewIssuesPage = () => {
+    const { register, control, handleSubmit } = useForm<IssueForm>()
+    const router = useRouter()
+
+    async function onSubmit(data: IssueForm) {
+        try {
+            const req = await fetch("/api/issues",
+                {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(data)
+                }
+            )
+            if (!req.ok) throw new Error()
+
+            router.push("/issues")
+        } catch (error) {
+
+        }
+    }
     return (
-        <div className={styles.textDiv}>
-            <TextField.Root placeholder="New Issue" className={styles.spaceBetween}></TextField.Root>
-            <SimpleMDE placeholder="Description" />
-            <Button >Submit New Issues</Button>
-        </div>
+        <form onSubmit={handleSubmit(onSubmit)} className={styles.textDiv}>
+            <TextField.Root className={styles.spaceBetween} placeholder="New Issue" {...register("title")} />
+            <Controller
+                name="description"
+                control={control}
+                render={({ field }) => <SimpleMDE placeholder="Description" {...field} />} />
+            <Button type="submit">Submit New Issues</Button>
+        </form >
     )
 }
 export default NewIssuesPage
