8d59331fee20a426fd11967e019a501c1eba378f
Edmond <edmondwu1022@gmail.com>
2025-04-17 15:37:22 +0800

    Build AlerDialog for delete issue

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,11 +1,26 @@
 import { RiDeleteBin6Line } from "react-icons/ri";
-import { Button } from "@radix-ui/themes"
+import { AlertDialog, Box, Button, Flex } from "@radix-ui/themes"
 import Link from "next/link"
-
 
 const DeleteIssueButton = ({ issueID }: { issueID: Number }) => {
     return (
-        <Button color="red"><RiDeleteBin6Line /><Link href={`/issues/${issueID}/edit`}>Delete Button</Link></Button>
+        <AlertDialog.Root>
+            <AlertDialog.Trigger>
+                <Button color="red"><RiDeleteBin6Line />Delete Button</Button>
+            </AlertDialog.Trigger>
+            <AlertDialog.Content>
+                <AlertDialog.Title>Comformation</AlertDialog.Title>
+                <AlertDialog.Description>Are you sure you want to delete this issue?</AlertDialog.Description>
+                <Flex gap={"4"} mt={"5"}>
+                    <AlertDialog.Cancel>
+                        <Button color="gray" >Cancel</Button>
+                    </AlertDialog.Cancel>
+                    <AlertDialog.Action>
+                        <Button color="red">Delete Issue</Button>
+                    </AlertDialog.Action>
+                </Flex>
+            </AlertDialog.Content>
+        </AlertDialog.Root>
     )
 }
 export default DeleteIssueButton
